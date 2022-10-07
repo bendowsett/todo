9aa171b2071bb5abe33926e5919fff12964c45c4
cs-mas <masako.gray@clearscore.com>
2022-10-07 17:19:02 +0100

    more mapping logics for ListView

--- app/src/main/java/com/benako/todo/ListFragment.kt
+++ app/src/main/java/com/benako/todo/ListFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.benako.todo.data.Todos
+import com.benako.todo.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var binding : FragmentListBinding
+
+    var exampleTodos = listOf(
+        Todos("Washing, based lamb"),
+        Todos("cooking, Running")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        binding.recyclerView.adapter = ListViewAdapter(exampleTodos)
+        return binding.root // root of the view from list fragment.
     }
 
 
