23f7d827f2a4ed4fb752157cac8f418b6e503e21
Ben Dowsett <ben.dowsett@clearscore.com>
2022-10-13 15:50:24 +0100

    Add checkbox and strikethrough

--- app/src/main/java/com/benako/todo/ListFragment.kt
+++ app/src/main/java/com/benako/todo/ListFragment.kt
@@ -1,47 +1,22 @@
 package com.benako.todo
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.GridLayoutManager
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.benako.todo.data.Todos
 import com.benako.todo.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
-    private lateinit var binding : FragmentListBinding
+    private lateinit var binding: FragmentListBinding
 
-    var exampleTodos = listOf(
-        Todos("Washing, based lamb"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running24rfhwoiruthvgiurghwiurhtgurt4hogwirthginrt"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running"),
-        Todos("cooking, Running")
-
-    )
+    var exampleTodos = mutableListOf<Todos>()
+    var adapter = ListViewAdapter(exampleTodos, ::deleteNote, ::uncheck)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,11 +24,33 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentListBinding.inflate(inflater, container, false)
-
+        var adapter = ListViewAdapter(exampleTodos, ::deleteNote, ::uncheck)
         binding.recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
-        binding.recyclerView.adapter = ListViewAdapter(exampleTodos)
+        binding.recyclerView.adapter = adapter
+
+        binding.addNoteButton.setOnClickListener {
+            addNote()
+            adapter.notifyDataSetChanged()
+
+        }
         return binding.root // root of the view from list fragment.
     }
 
+    fun addNote() {
+        var input = binding.noteInput.text.toString()
+        exampleTodos.add(Todos(input))
+        binding.noteInput.text.clear()
 
+    }
+
+    fun deleteNote(text: TextView) {
+        text.paintFlags = text.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+    }
+
+    fun uncheck(text: TextView) {
+        text.setPaintFlags(0)
+    }
 }
+
+
+
